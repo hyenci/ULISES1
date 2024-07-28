14b99df81a78eb165521ff1e7e36d2b38b81713f
hyenci21 <159926659+hyenci21@users.noreply.github.com>
2024-07-27 18:10:26 -0600

    TRABAJO EQUIPO

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import vehiculos.tipoCarro;
+public class Main {
+    public static void main(String[] args) {
+         tipoCarro carro = new tipoCarro("Mazda","Caca","2020");
+        carro.mostrarTipoCarro();
+
+    }
+}
